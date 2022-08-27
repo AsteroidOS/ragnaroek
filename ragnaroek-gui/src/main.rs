8a1a96be4b976690ba11930d97a980e1fb2f4b0a
casept <davids.paskevics@gmail.com>
2022-08-27 14:22:10 +0200

    Some UI rework

--- ragnaroek-gui/src/main.rs
+++ ragnaroek-gui/src/main.rs
@@ -1,30 +1,158 @@
-//! This crate provides an FLTK-based graphical frontend for ragnaroek.
+//! This crate provides an egui-based graphical frontend for ragnaroek.
 
-use fltk::{app::App, button::Button, prelude::*, window::Window};
-use fltk_theme::{widget_themes, ThemeType, WidgetTheme};
+mod pit_ui;
+
+use eframe::egui;
+use pit;
+use ragnaroek;
+use std::{
+    fs,
+    path::PathBuf,
+    sync::mpsc::{self, TryRecvError},
+};
+
+/// All the Odin .ini files I could find only ever mention this port
+const WIRELESS_PORT: u16 = 13579;
+/// All the targets implementing wireless mode seem to use this IP
+const WIRELESS_TARGET_IP: &str = "192.168.49.1";
 
 fn main() {
-    let app = App::default();
-    // Choose a theme to match system dark mode state
-    // TODO: Re-check and re-theme regularly rather than only at app start
-    let widget_theme = match dark_light::detect() {
-        dark_light::Mode::Light => WidgetTheme::new(ThemeType::Classic),
-        dark_light::Mode::Dark => WidgetTheme::new(ThemeType::Dark),
-    };
-    widget_theme.apply();
-
-    let mut win = Window::default().with_size(400, 300);
-    let mut btn = Button::new(160, 200, 80, 30, "Print PIT");
-    btn.set_frame(widget_themes::OS_DEFAULT_BUTTON_UP_BOX);
-    btn.set_callback(move |_| on_print_pit());
-
-    // Create a box at the bottom of the screen showing log output
-
-    win.end();
-    win.show();
-    app.run().unwrap();
+    let mut native_options = eframe::NativeOptions::default();
+    native_options.follow_system_theme = true;
+    eframe::run_native(
+        "Ragnaroek",
+        native_options,
+        Box::new(|cc| Box::new(RagnaroekApp::new(cc))),
+    );
 }
 
-fn on_print_pit() {
-    println!("Pressed!");
+/// How to communicate with the target.
+
+#[derive(PartialEq, Default, Copy, Clone)]
+enum CommsMode {
+    #[default]
+    Usb,
+    NetBind,
+    NetConnect,
+}
+
+#[derive(Default)]
+struct RagnaroekApp {
+    comms_mode: CommsMode,
+    comms_mode_radio_enabled: bool,
+    conn: Option<Box<dyn ragnaroek::Communicator>>,
+    pit_dialog_receiver: Option<mpsc::Receiver<Option<PathBuf>>>,
+    rendering_pit: Option<pit::Pit>,
+}
+
+impl RagnaroekApp {
+    fn new(_: &eframe::CreationContext<'_>) -> Self {
+        let mut s = Self::default();
+        s.comms_mode_radio_enabled = true;
+        return s;
+    }
+}
+
+fn connect(m: CommsMode) -> ragnaroek::Result<Box<dyn ragnaroek::Communicator>> {
+    use CommsMode::*;
+    match m {
+        Usb => {
+            let c = ragnaroek::UsbConnection::establish()?;
+            return Ok(Box::new(c));
+        }
+        NetBind => {
+            let mut l = ragnaroek::NetBindListener::new(WIRELESS_PORT);
+            let c = l.accept()?;
+            return Ok(Box::new(c));
+        }
+        NetConnect => {
+            let c = ragnaroek::NetConnectConnection::new(WIRELESS_TARGET_IP, WIRELESS_PORT);
+            return Ok(Box::new(c));
+        }
+    }
+}
+
+impl eframe::App for RagnaroekApp {
+    fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.vertical_centered_justified(|ui| {
+                ui.heading("Ragnaroek");
+            });
+
+            ui.vertical(|ui| {
+                ui.horizontal(|ui| {
+                    ui.vertical(|ui| {
+                        // Allow user to pick how to communicate with device
+                        ui.heading("Communications mode");
+                        for (mode, text) in [
+                            (CommsMode::Usb, "USB"),
+                            (CommsMode::NetBind, "Net (Bind)"),
+                            (CommsMode::NetConnect, "Net (Connect)"),
+                        ] {
+                            let rbtn = egui::RadioButton::new(self.comms_mode == mode, text);
+                            if ui
+                                .add_enabled(self.comms_mode_radio_enabled, rbtn)
+                                .clicked()
+                            {
+                                self.comms_mode = mode;
+                            }
+                        }
+                    });
+                    // Button for establishing connection
+                    if ui.button("Detect").clicked() {
+                        let c = connect(self.comms_mode).unwrap();
+                        self.conn = Some(c);
+                    }
+
+                    // Label displaying connection status
+                    if self.conn.is_some() {
+                        ui.colored_label(egui::Color32::GREEN, "Connected: YES ✓");
+                    } else {
+                        ui.colored_label(egui::Color32::RED, "Connected: NO ❌");
+                    }
+                });
+
+                // Print PIT from device
+                let print_pit_btn = egui::Button::new("Print PIT");
+                if ui.add_enabled(self.conn.is_some(), print_pit_btn).clicked() {
+                    let conn = self.conn.as_mut().unwrap();
+                    self.rendering_pit = Some(pit_ui::download(ui, conn));
+                }
+
+                // Parse PIT from file
+                if ui.button("Parse PIT from file").clicked() {
+                    self.pit_dialog_receiver = Some(pit_ui::open_dialog());
+                }
+                // In order to avoid blocking the main thread, file dialog is displayed in a worker thread.
+                // Wait for it's result here.
+                if self.pit_dialog_receiver.is_some() {
+                    let r = self.pit_dialog_receiver.as_mut().unwrap();
+                    match r.try_recv() {
+                        // Have path, set PIT to render
+                        Ok(Some(path)) => {
+                            self.pit_dialog_receiver = None;
+                            let pit = fs::read(path).unwrap();
+                            let pit = pit::Pit::deserialize(&pit).unwrap();
+                            self.rendering_pit = Some(pit);
+                        }
+                        // User cancelled, we're done
+                        Ok(None) => {
+                            self.pit_dialog_receiver = None;
+                        }
+                        // Nothing happened yet, keep waiting
+                        Err(TryRecvError::Empty) => {}
+                        // Disconnection, shouldn't happen
+                        Err(TryRecvError::Disconnected) => {
+                            panic!("Disconnect");
+                        }
+                    }
+                }
+                // If there's a PIT file to render, do it
+                if self.rendering_pit.is_some() {
+                    let p = self.rendering_pit.as_ref().unwrap();
+                    pit_ui::draw_table(ui, p.clone());
+                }
+            });
+        });
+    }
 }
---
+++ ragnaroek-gui/src/pit_ui.rs
@@ -0,0 +1,88 @@
+use eframe::egui;
+use egui_extras::TableBuilder;
+use pit;
+use ragnaroek;
+use rfd;
+
+use std::path::PathBuf;
+use std::sync::mpsc;
+use std::thread;
+
+pub fn download(ui: &mut egui::Ui, c: &mut Box<dyn ragnaroek::Communicator>) -> pit::Pit {
+    ragnaroek::download_protocol::begin_session(c).unwrap();
+    let pit = ragnaroek::download_protocol::download_pit(c).unwrap();
+    return pit;
+}
+
+/// Ask the user to pick a PIT file.
+/// Does not block.
+/// Instead, result is returned as a message on the returned channel when ready.
+/// If a `None` is received over the channel, the user cancelled.
+pub fn open_dialog() -> mpsc::Receiver<Option<PathBuf>> {
+    let (send, recv) = mpsc::channel();
+    thread::spawn(move || {
+        let path = rfd::FileDialog::new()
+            .add_filter("PIT", &["pit"])
+            .pick_file();
+        send.send(path).unwrap();
+    });
+    return recv;
+}
+
+/// Draw the PIT contents as a table.
+pub fn draw_table(ui: &mut egui::Ui, pit: pit::Pit) {
+    // Not part of table, but related
+    ui.vertical(|ui| {
+        ui.label(format!("Gang Name: {}", pit.gang_name));
+        ui.label(format!("Project Name: {}", pit.project_name));
+        ui.label(format!("Version: {}", pit.proto_version));
+    });
+    let headings = [
+        "Type",
+        "Device Type",
+        "ID",
+        "Attributes",
+        "Update Attributes",
+        "Block Size / Offset",
+        "Block Count",
+        "File Offset",
+        "File Size",
+        "Partition Name",
+        "Flash Filename",
+        "FOTA Filename",
+    ];
+    TableBuilder::new(ui)
+        .resizable(true)
+        .columns(egui_extras::Size::remainder(), headings.len())
+        .header(60.0, |mut header| {
+            for heading in headings {
+                header.col(|ui| {
+                    ui.heading(heading);
+                });
+            }
+        })
+        .body(|mut body| {
+            for entry in pit {
+                body.row(25.0, |mut row| {
+                    for text in [
+                        format!("{}", entry.pit_type),
+                        format!("{}", entry.pit_device_type),
+                        format!("{}", entry.pit_id),
+                        format!("{:?}", entry.pit_attributes),
+                        format!("{:?}", entry.pit_update_attributes),
+                        format!("{}", entry.block_size_or_offset),
+                        format!("{}", entry.block_count),
+                        format!("{}", entry.file_offset),
+                        format!("{}", entry.file_size),
+                        entry.partition_name.clone(),
+                        entry.flash_filename.clone(),
+                        entry.fota_filename.clone(),
+                    ] {
+                        row.col(|ui| {
+                            ui.label(text);
+                        });
+                    }
+                });
+            }
+        });
+}
