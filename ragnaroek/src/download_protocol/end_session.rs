d68137b4ecf3bc594375388e1ea83911cfa713c6
casept <davids.paskevics@gmail.com>
2023-01-30 13:55:33 +0100

    core: Slightly enhance session logging

--- ragnaroek/src/download_protocol/end_session.rs
+++ ragnaroek/src/download_protocol/end_session.rs
@@ -7,6 +7,7 @@ const REBOOT: u32 = 0x01;
 
 /// Ends the targets session, with an optional reboot to the OS.
 pub fn end_session(c: &mut Box<dyn Communicator>, reboot: bool) -> Result<()> {
+    log::debug!(target: "SESS", "Ending session");
     // Heimdall always first sends a session end, and only then a reboot.
     // Not sure if needed or we could send a reboot request immediately.
     let p = OdinCmdPacket::with_1_arg(OdinCmd::SessionEnd, OdinInt::from(END_SESSION));
@@ -17,8 +18,10 @@ pub fn end_session(c: &mut Box<dyn Communicator>, reboot: bool) -> Result<()> {
     if resp.cmd != OdinCmd::SessionEnd {
         return Err(DownloadProtocolError::UnexpectedOdinCmd(OdinCmd::SessionEnd, resp.cmd).into());
     }
+    log::debug!(target: "SESS", "Ending session OK");
 
     if reboot {
+        log::debug!(target: "SESS", "Rebooting target");
         // Send a reboot
         let p = OdinCmdPacket::with_1_arg(OdinCmd::SessionEnd, OdinInt::from(REBOOT));
         p.send(c)?;
@@ -29,6 +32,7 @@ pub fn end_session(c: &mut Box<dyn Communicator>, reboot: bool) -> Result<()> {
                 DownloadProtocolError::UnexpectedOdinCmd(OdinCmd::SessionEnd, resp.cmd).into(),
             );
         }
+        log::debug!(target: "SESS", "Rebooting target OK");
     }
 
     return Ok(());
